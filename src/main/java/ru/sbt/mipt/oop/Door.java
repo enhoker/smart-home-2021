bdb3472286f74e93157b96d78fa5f779cb3f6e7f
enhoker <45732134+enhoker@users.noreply.github.com>
2021-03-09 20:14:08 +0300

    Update Door.java
    Добавлена функция getIsOpen()

--- src/main/java/ru/sbt/mipt/oop/Door.java
+++ src/main/java/ru/sbt/mipt/oop/Door.java
@@ -12,6 +12,10 @@ public class Door {
     public String getId() {
         return id;
     }
+    
+    public boolean getIsOpen() {
+        return isOpen;
+    }
 
     public void setOpen(boolean open) {
         isOpen = open;
