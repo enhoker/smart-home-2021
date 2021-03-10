03aabc5f6bc9499ae479090206b1c1634f1bd50a
enhoker <45732134+enhoker@users.noreply.github.com>
2021-03-10 22:28:23 +0300

    Add files via upload

---
+++ src/main/java/ru/sbt/mipt/oop/HomeOut.java
@@ -0,0 +1,25 @@
+package ru.sbt.mipt.oop;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class HomeOut {
+
+    public static void outHome(SmartHome smartHome) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String jsonString = gson.toJson(smartHome);
+        System.out.println(jsonString);
+        Path path = Paths.get("output.js");
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            writer.write(jsonString);
+        }
+    }
+
+}
